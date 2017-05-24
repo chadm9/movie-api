f9cb6f18707ec03a3dbee448c156d6ba771cc70a
Chad McKee <chadm_@yahoo.com>
2017-05-24 14:45:10 -0400

    modal

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,41 @@ $(document).ready(function () {
     const apiBaseUrl = 'http://api.themoviedb.org/3';
     const imageBaseUrl = 'http://image.tmdb.org/t/p/';
     const nowPlayingURL = apiBaseUrl + '/movie/now_playing?api_key=' + apiKey;
-    console.log(nowPlayingURL);
 
     $.getJSON(nowPlayingURL,function (nowPlayingData) {
-        console.log(nowPlayingData);
+        //console.log(nowPlayingData);
 
+/*
         nowPlayingHTML = '';
         for(var i = 0; i < nowPlayingData.results.length; i++){
             var posterURL = imageBaseUrl + 'w300'  + nowPlayingData.results[i].poster_path;
             nowPlayingHTML += '<div class="col-sm-6 col-sm-3">';
             nowPlayingHTML += '<img src="' + posterURL + '">';
             nowPlayingHTML += '</div>';
+*/
+
+
+        $('#movie-grid').html(getHTML(nowPlayingData));
 
 
 
-        }
-        $('#movie-grid').html(nowPlayingHTML);
+
+        $('.movie-poster').click(function(){
+            // Change teh HTML inside the modal
+            var thisMovieId = $(this).attr('movie-id');
+            //console.log(thisMovieId);
+            var thisMovieUrl = `${apiBaseUrl}/movie/${thisMovieId}?api_key=${apiKey}`;
+            $.getJSON(thisMovieUrl,(thisMovieData)=>{
+                //console.log(thisMovieData);
+                $('#myModalLabel').html(thisMovieData.title);
+            $('.modal-body').html(thisMovieData.overview);
+            // Open teh modal
+                $("#myModal").modal();
+            });
+        });
+
+
+
     });
 
     $('#movie-form').submit(function (event) {
@@ -30,9 +49,22 @@ $(document).ready(function () {
        //console.log(searchURL);
 
         $.getJSON(searchURL, function (searchMovieData) {
+            console.log($);
             var searchMovieHTML = getHTML(searchMovieData);
             $('#movie-grid').html(searchMovieHTML);
-
+            $('.movie-poster').click(function(){
+                // Change teh HTML inside the modal
+                var thisMovieId = $(this).attr('movie-id');
+                //console.log(thisMovieId);
+                var thisMovieUrl = `${apiBaseUrl}/movie/${thisMovieId}?api_key=${apiKey}`;
+                $.getJSON(thisMovieUrl,(thisMovieData)=>{
+                    //console.log(thisMovieData);
+                    $('#myModalLabel').html(thisMovieData.title);
+                    $('.modal-body').html(thisMovieData.overview);
+                // Open teh modal
+                    $("#myModal").modal();
+                });
+            });
 
         })
 
@@ -41,14 +73,20 @@ $(document).ready(function () {
 
     function getHTML(nowPlayingData) {
         var nowPlayingHTML = '';
+        //console.log(nowPlayingData.results.length);
+        //console.log(nowPlayingData);
+
+
         for(var i = 0; i < nowPlayingData.results.length; i++) {
             var posterURL = imageBaseUrl + 'w300' + nowPlayingData.results[i].poster_path;
-            nowPlayingHTML += '<div class="col-sm-6 col-sm-3">';
+            nowPlayingHTML += '<div class="col-md-3 col-sm-6 movie-poster" movie-id=' + nowPlayingData.results[i].id + '>';
             nowPlayingHTML += '<img src="' + posterURL + '">';
             nowPlayingHTML += '</div>';
 
-            return nowPlayingHTML;
+
 
         }
+        return nowPlayingHTML;
+
     }
 });
